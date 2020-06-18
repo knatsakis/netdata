9e53872c63b5df5df0367f9ac14cade1f433e36e
Konstantinos Natsakis <5933427+knatsakis@users.noreply.github.com>
2020-06-18 09:47:06 +0300

    Modify packaging/docker/run.sh to auto claim

--- packaging/docker/run.sh
+++ packaging/docker/run.sh
@@ -5,7 +5,15 @@
 # Copyright: SPDX-License-Identifier: GPL-3.0-or-later
 #
 # Author  : Pavlos Emm. Katsoulakis <paul@netdata.cloud>
-set -e
+install -m 0770 -o netdata -g netdata -d /var/lib/netdata/registry
+uuidgen --sha1 --namespace @dns --name $HOSTNAME > /var/lib/netdata/registry/netdata.public.unique.id
+
+chmod 0670 /var/lib/netdata/registry/netdata.public.unique.id
+chown netdata:netdata /var/lib/netdata/registry/netdata.public.unique.id
+
+/usr/sbin/netdata-claim.sh -token="${CLAIM_TOKEN}" -rooms="${CLAIM_ROOM}" -url="${ACLK_URL}"
+
+chown -R netdata:netdata /etc/netdata/claim.d
 
 if [ ! "${DO_NOT_TRACK:-0}" -eq 0 ] || [ -n "$DO_NOT_TRACK" ]; then
   touch /etc/netdata/.opt-out-from-anonymous-statistics
@@ -24,6 +32,6 @@ if [ -n "${PGID}" ]; then
   usermod -a -G "${PGID}" "${DOCKER_USR}" || echo >&2 "Could not add netdata user to group docker with ID ${PGID}"
 fi
 
-exec /usr/sbin/netdata -u "${DOCKER_USR}" -D -s /host -p "${NETDATA_PORT}" -W set web "web files group" root -W set web "web files owner" root "$@"
+exec /usr/sbin/netdata -u "${DOCKER_USR}" -D -s /host -p "${NETDATA_PORT}" -W set cloud "cloud base url" ${ACLK_URL} -W set global "errors flood protection period" 0 -W set web "web files group" root -W set web "web files owner" root "$@"
 
 echo "Netdata entrypoint script, completed!"
